bb4aef491be688d5905a87896cc07c3c27028cdf
Nicolai Bøttkjær <nbottkjaer@gmail.com>
2024-10-29 22:47:01 +0100

    Initial Commit

---
+++ OneBigProject/OneBigProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneBigProjectApp.swift
+//  OneBigProject
+//
+//  Created by Nicolai Bøttkjær on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct OneBigProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
